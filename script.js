b753cf5a51bbf2d69d54a2c547d78fb54487a45c
PINHOf <fpinhocontact@gmail.com>
2023-12-26 19:27:13 +0000

    Adapted and improved to new google results

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         OldGoogleIsBetter
 // @namespace    google
-// @version      0.1
+// @version      0.2
 // @description  Old google results is better
 // @author       PINHOf
 // @license      MIT
@@ -12,124 +12,192 @@
 // @run-at       document-start
 // ==/UserScript==
 
-(function() {
-    'use strict';
 
-    var prevResultCount = 0;
-    var bettered = false;
+// Starts observing the search results
+function startObservingResults(cl)
+{
+	console.log('OldGoogleIsBetter: starting to observe results');
 
-    checkElementThenRun('#rcnt', run);
+	const observer = new MutationObserver(function(mutations)
+	{
+		const el = document.querySelector(`${cl}:not(.old-google-is-better)`);
 
-    // Loops through all the results rows & watches for changes in the main div
-    // Function extracted from https://github.com/aligo/better-google
-    function run()
-    {
-        if (prevResultCount != document.querySelectorAll('.MjjYud').length)
-        {
-            document.querySelectorAll('.MjjYud').forEach(row);
-            prevResultCount = document.querySelectorAll('.MjjYud').length;
-        }
+		if (!el)
+			return;
 
-        if (!bettered)
-        {
-            if (MutationObserver != undefined)
-            {
-                var searchEl = document.getElementById('rcnt');
-                var observer = new MutationObserver(run);
-                observer.observe(searchEl, {childList: true, subtree: true});
-            }
+		console.log('OldGoogleIsBetter: found search container');
+		
+		// Applies a class to the element
+		el.classList.add('old-google-is-better');
+		
+		beautifyResults(el);
+	});
 
-            bettered = true;
-        }
-    }
+	const config = { subtree: true, childList: true };
+	observer.observe(document, config);
+}
 
-    // Checks if certain element exists and runs the function associated
-    // Function extracted from https://github.com/aligo/better-google
-    function checkElementThenRun(selector, func)
-    {
-        var el = document.querySelector(selector);
+// Starts observing the search results feedback container
+function startObservingFeedback()
+{
+	console.log('OldGoogleIsBetter: starting to observe feedback');
 
-        if (el != null)
-        {
-            func();
-            return;
-        }
+	const observer = new MutationObserver(function(mutations)
+	{
+		const el = document.querySelector('.Wt5Tfe');
 
-        if (window.requestAnimationFrame != undefined)
-            window.requestAnimationFrame(function(){ checkElementThenRun(selector, func)});
-        else
-        {
-            document.addEventListener('readystatechange', function(e)
-            {
-                if (document.readyState == 'complete')
-                    func();
-            });
-        }
-    }
+		if (!el)
+			return;
 
-    // Formats the row / result to the old style
-    function row(el)
-    {
-        const url = getUrl(el);
+		console.log('OldGoogleIsBetter: found search feedback container');
+		
+		// Removes the entire feedback container
+		el.closest('.TzHB6b').remove();
+		
+		console.log('OldGoogleIsBetter: removed search feedback container');
+		
+		observer.disconnect();
+	});
 
-        removeHeaderInformation(el);
-        removeUnnecessaryLineBreak(el);
-        adjustTitleMargin(el);
-        appendUrlAfterTitle(el, url);
-    }
+	const config = { subtree: true, childList: true };
+	observer.observe(document, config);
+}
 
-    // Extracts the URL from the <a href> result
-    function getUrl(el)
-    {
-        const linkEl = el.querySelector('a');
-        const url = linkEl ? linkEl.getAttribute('href') : '';
+// Starts observing the search results images container
+function startObservingImages()
+{
+	console.log('OldGoogleIsBetter: starting to observe images');
 
-        return url;
-    }
+	const observer = new MutationObserver(function(mutations)
+	{
+		const el = document.querySelector('[jsmodel="Wn3aEc"]');
 
-    // Removes the header information (icon; url; three dots; etc)
-    function removeHeaderInformation(el)
-    {
-        el.querySelectorAll('.TbwUpd').forEach((elem) => elem.remove());
-        el.querySelectorAll('.B6fmyf').forEach((elem) => elem.remove());
-    }
+		if (!el)
+			return;
 
-    // Removes extra <br> tags
-    function removeUnnecessaryLineBreak(el)
-    {
-        const link = el.querySelector('a');
+		console.log('OldGoogleIsBetter: found search images container');
+		
+		// Removes the entire images container
+		el.closest('.TzHB6b').remove();
+		
+		console.log('OldGoogleIsBetter: removed search images container');
+		
+		observer.disconnect();
+	});
 
-        if (link)
-            link.querySelectorAll('br').forEach((elem) => elem.remove());
-    }
+	const config = { subtree: true, childList: true };
+	observer.observe(document, config);
+}
 
-    // Adjusts the title margin top (after the header information has been removed from DOM)
-    function adjustTitleMargin(el)
-    {
-        const titleEl = el.querySelector('.LC20lb');
+// Starts observing the search results related container
+function startObservingRelated()
+{
+	console.log('OldGoogleIsBetter: starting to observe related');
 
-        if (titleEl)
-            titleEl.style.marginTop = '0px';
-    }
+	const observer = new MutationObserver(function(mutations)
+	{
+		const el = document.querySelector('.mgAbYb');
 
-    // Appends the URL after the title in a green color
-    function appendUrlAfterTitle(el, url)
-    {
-        const titleParentEl = el.querySelector('.yuRUbf');
-        const newEl = document.createElement('a');
-        newEl.setAttribute('href', url);
-        newEl.innerHTML = url;
-        newEl.style.color = '#006621';
-        newEl.style.fontSize = '15px';
-        newEl.style.display = 'block';
+		if (!el)
+			return;
 
-        if (titleParentEl)
-        {
-            const parent = titleParentEl.parentElement;
-            const totalLinks = parent.querySelectorAll('a').length;
+		console.log('OldGoogleIsBetter: found search related container');
+		
+		// Removes the entire related container
+		el.closest('.TzHB6b').remove();
+		
+		console.log('OldGoogleIsBetter: removed search related container');
+		
+		observer.disconnect();
+	});
 
-           if (totalLinks == 1)
-               titleParentEl.after(newEl);
-        }
-    }
+	const config = { subtree: true, childList: true };
+	observer.observe(document, config);
+}
+
+// Beautifies all the results as old google
+function beautifyResults(el)
+{
+	console.log('OldGoogleIsBetter: beautifying results');
+	
+	const url = getUrl(el);
+		
+	removeHeaderInformation(el);
+	removeUnnecessaryLineBreak(el);
+	adjustTitleMargin(el);
+	appendUrlAfterTitle(el, url);
+}
+
+// Extracts the URL from the <a href> result
+function getUrl(el)
+{
+	const linkEl = el.querySelector('a');
+	const url = linkEl ? linkEl.getAttribute('href') : '';
+	
+	console.log(`OldGoogleIsBetter: extracting url ${url}`);
+
+	return url;
+}
+
+// Removes the header information (icon; url; three dots; etc)
+function removeHeaderInformation(el)
+{
+	el.querySelectorAll('.TbwUpd').forEach((elem) => elem.remove());
+	el.querySelectorAll('.B6fmyf').forEach((elem) => elem.remove());
+	
+	console.log('OldGoogleIsBetter: removing header information');
+}
+
+// Removes extra <br> tags
+function removeUnnecessaryLineBreak(el)
+{
+	const link = el.querySelector('a');
+
+	if (link)
+		link.querySelectorAll('br').forEach((elem) => elem.remove());
+	
+	console.log('OldGoogleIsBetter: removing unecessary <br> tags');
+}
+
+// Adjusts the title margin top (after the header information has been removed from DOM)
+function adjustTitleMargin(el)
+{
+	const titleEl = el.querySelector('.LC20lb');
+
+	if (titleEl)
+		titleEl.style.marginTop = '0px';
+	
+	console.log('OldGoogleIsBetter: adjusting title margin');
+}
+
+// Appends the URL after the title in a green color
+function appendUrlAfterTitle(el, url)
+{
+	const titleParentEl = el.querySelector('.yuRUbf');
+	const newEl = document.createElement('a');
+	newEl.setAttribute('href', url);
+	newEl.innerHTML = url;
+	newEl.style.color = '#006621';
+	newEl.style.fontSize = '15px';
+	newEl.style.display = 'block';
+
+	if (titleParentEl)
+	{
+		const parent = titleParentEl.parentElement;
+		const totalLinks = parent.querySelectorAll('a').length;
+
+	   if (totalLinks == 1)
+		   titleParentEl.after(newEl);
+	}
+	
+	console.log('OldGoogleIsBetter: appending url after title');
+}
+
+(function()
+{
+	startObservingResults('.TzHB6b');
+	startObservingResults('.MjjYud');
+	startObservingFeedback();
+	startObservingImages();
+	startObservingRelated();
 })();
